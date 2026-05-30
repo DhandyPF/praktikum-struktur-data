330cd461fc6e71598a7b0e099f62ea42d8b1f420
DhandyPF <dhandy.pf@gmail.com>
2026-05-30 20:32:07 +0700

    Pembuatan hasil pengerjaan responsi Praktikum Strutur Data

--- Praktikum/Responsi/responsi.cpp
+++ Praktikum/Responsi/responsi.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+typedef struct node *address;
+struct node{
+    int data;
+    address next;
+};
+
+typedef struct {
+    address front;
+    address rear;
+} Queue;
+
+Queue antrian;
+
+address alokasi(int nilai) {
+    address baru = new node;
+    baru->data = nilai;
+    baru->next = NULL;
+    return baru;
+}
+
+int main() {
+
+}
